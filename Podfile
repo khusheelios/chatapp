f285b345989219bf6d87fda9eabdd420952adb45
khusheel <khusheel.verma@zanjo.io>
2020-01-26 21:09:38 +0530

    lesson 2

---
+++ Podfile
@@ -0,0 +1,10 @@
+
+use_frameworks!
+
+target 'ChatAppDemo' do
+  
+	pod 'Alamofire’
+	pod 'SwiftyJSON', '~> 4.0'
+	pod 'Socket.IO-Client-Swift'
+
+end
---
+++ Pods/Target Support Files/Pods-ChatAppDemo/Pods-ChatAppDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChatAppDemo : NSObject
+@end
+@implementation PodsDummy_Pods_ChatAppDemo
+@end
