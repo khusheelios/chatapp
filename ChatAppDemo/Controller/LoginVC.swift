340556740425c58f10be44a9f147e6d3131ace71
khusheel <khusheel.verma@zanjo.io>
2020-01-26 22:32:25 +0530

    LoginDesign

---
+++ ChatAppDemo/Controller/LoginVC.swift
@@ -0,0 +1,35 @@
+//
+//  LoginVC.swift
+//  ChatAppDemo
+//
+//  Created by Zanjo iOS Dev on 26/01/20.
+//  Copyright © 2020 Zanjo iOS Dev. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func crossbtnpressed(_ sender: Any)
+    {
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ChatAppDemo/Controller/SlideMenuVC.swift
+++ ChatAppDemo/Controller/SlideMenuVC.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class SlideMenuVC: UIViewController {
 
+   
     
-    
+    @IBOutlet weak var btnLogin: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,15 +21,14 @@ class SlideMenuVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   
+    @IBAction func loginbtnPressed(_ sender: Any)
+    {
+        
     }
-    */
+    
+    
+    
+    
 
 }
